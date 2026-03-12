6457a5e672fd63c129ebcb2150fc949e965d7f25
João Fernandes <joaopfernandes28@gmail.com>
2026-03-12 17:26:44 +0000

    tirei add()

--- src/main/java/ipleiria/estg/dei/ei/esoft/HelloWorld.java
+++ src/main/java/ipleiria/estg/dei/ei/esoft/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld extends JFrame {
         setMinimumSize(new Dimension(300, 200));
         JLabel lblMensagem2 = new JLabel("Joao Fernandes esteve aqui!"); //minha linha de code
     //AJAJJSSKALMAXLS
-        painelPrincipal.add(lblMensagem2);
+        //painelPrincipal.add(lblMensagem2);
         setContentPane(painelPrincipal);
         pack();
     }

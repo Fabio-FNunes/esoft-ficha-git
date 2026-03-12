35310eb8b9fa3954a0c34d5476c1be866b5481ad
Fábio Nunes <2231014@my.ipleiria.pt>
2026-03-12 17:23:36 +0000

    aaa

--- src/main/java/ipleiria/estg/dei/ei/esoft/HelloWorld.java
+++ src/main/java/ipleiria/estg/dei/ei/esoft/HelloWorld.java
@@ -5,12 +5,14 @@ import java.awt.*;
 
 public class HelloWorld extends JFrame {
     private JPanel painelPrincipal;
+    private JLabel lblMensagem2;
 
     public HelloWorld() {
         setMinimumSize(new Dimension(300, 200));
         JLabel lblMensagem2 = new JLabel("Joao Fernandes esteve aqui!"); //minha linha de code
 
         painelPrincipal.add(lblMensagem2);
+        //AAAAAAAAAAAAAAAAAAAAAAAAa
         setContentPane(painelPrincipal);
         pack();
     }
